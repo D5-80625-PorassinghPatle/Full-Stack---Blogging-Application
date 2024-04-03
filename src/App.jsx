22faf9ca9cd03adf7063fb53936d8fdebb01cc9f
D5-80625-PorassinghPatle <poraspatle@gmail.com>
2024-04-03 16:51:52 +0530

    initial redux store configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import "./App.css";
+import authService from "./appWrite/auth.js";
+import { login, logout } from "./store/authSlice.js";
+import { Outlet } from "react-router-dom";
+
 
 function App() {
+  //we will create a Loading state as the databse is not local to machine
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>
-        A Blog With AppWrite
-      </h1>
-    </>
-  )
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-500">
+      <div className="w-full block">
+        
+        <main>{/*<Outlet/> */}</main>
+        
+      </div>
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;

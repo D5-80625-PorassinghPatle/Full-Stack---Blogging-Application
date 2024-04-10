6447fca61b07b2f44f88b8943ad546a34f9a947d
D5-80625-PorassinghPatle <poraspatle@gmail.com>
2024-04-10 08:08:26 +0530

    created pages, working on debugging Network request error

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,38 @@
-import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import "./App.css";
-import authService from "./appWrite/auth.js";
-import { login, logout } from "./store/authSlice.js";
-import { Outlet } from "react-router-dom";
-import { Header } from "./components/index.js";
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import './App.css'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  //we will create a Loading state as the databse is not local to machine
-  const [loading, setLoading] = useState(true);
-  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    authService
-      .getCurrentUser()
-      .then((userData) => {
-        if (userData) {
-          dispatch(login({ userData }));
-        } else {
-          dispatch(logout());
-        }
-      })
-      .finally(() => setLoading(false));
-  }, []);
-
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
   return !loading ? (
-    <div className="min-h-screen flex flex-wrap content-between bg-gray-500">
-      <div className="w-full block">
-      <Header></Header>
-        <main>{/*<Outlet/> */}</main>
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:  <Outlet />
+        </main>
+        <Footer />
       </div>
     </div>
-  ) : null;
+  ) : null
 }
 
-export default App;
+export default App
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,16 @@
-import React, { Children } from "react";
+import React from "react";
 
 export default function Button({
-  children,
-  type = "button",
-  bgColor = "bg-blue-900",
-  textColor = "text-white",
-  className = "",
-  ...props
+    children,
+    type = "button",
+    bgColor = "bg-blue-600",
+    textColor = "text-white",
+    className = "",
+    ...props
 }) {
-  return (
-    <button
-      className={`px-4 py-2 rounded-lg${textColor} ${bgColor} ${className}`}
-      ${...props}>
-      {children}
-    </button>
-  );
+    return (
+        <button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`} {...props}>
+            {children}
+        </button>
+    );
 }
